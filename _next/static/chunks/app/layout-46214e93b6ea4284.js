b70998d1dd2dcc2066804d52c67cb42c959946ed
tomasmaillo <38633386+tomasmaillo@users.noreply.github.com>
2024-05-30 17:15:03 +0000

    Deploying to public from @ compsoc-edinburgh/comp-soc.com@de9638eb154f7ff7ff57cb7a1d2a46451d5ff2d5 🚀

--- _next/static/chunks/app/layout-46214e93b6ea4284.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7118:function(e,n,t){Promise.resolve().then(t.t.bind(t,8877,23)),Promise.resolve().then(t.t.bind(t,8173,23)),Promise.resolve().then(t.t.bind(t,231,23))},8877:function(){}},function(e){e.O(0,[404,173,231,971,23,744],function(){return e(e.s=7118)}),_N_E=e.O()}]);
