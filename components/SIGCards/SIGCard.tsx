a7d278b2eddedb111ac40d7f4102f1541e2c6ff6
tomas <tomasroma64@gmail.com>
2024-05-31 21:37:49 +0100

    back to Next Image

--- components/SIGCards/SIGCard.tsx
+++ components/SIGCards/SIGCard.tsx
@@ -24,7 +24,7 @@ const SIGCard = ({ sig }: { sig: SIG }) => {
       )}
       <a href={sig.websiteURL} target="_blank" rel="noreferrer">
         <div className="flex flex-col justify-between h-full">
-          <img
+          <Image
             src={`${prefix}/${sig.icon.src}`}
             alt={sig.icon.alt}
             className={`w-24 h-24 mb-12 ${
