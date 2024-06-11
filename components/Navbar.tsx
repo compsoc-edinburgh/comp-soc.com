68e6f611d915d5c9b83cbb1aecfcb605aa27d9f1
tomas <tomasroma64@gmail.com>
2024-06-11 12:33:33 +0100

    fix navbar routing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     <nav className="sticky top-0 z-50 container mx-auto p-4 max-w-screen-lg">
       <div className="container bg-foreground mx-auto flex justify-between items-center p-4 border-border border-2 rounded-sm">
         <div className="text-white font-bold">
-          <Link href="/">
+          <Link href={`${prefix}/`}>
             <Image
               src={`${prefix}/compsoc-short.png`}
               alt="Wide CompSoc logo"
@@ -18,9 +18,9 @@ const NavBar = () => {
           </Link>
         </div>
         <div className="flex space-x-8 px-2">
-          <Link href="/team">Team</Link>
-          <Link href="/events">Events</Link>
-          <Link href="/about">About</Link>
+          <Link href={`${prefix}/team`}>Team</Link>
+          <Link href={`${prefix}/events`}>Events</Link>
+          <Link href={`${prefix}/about`}>About</Link>
         </div>
       </div>
     </nav>
