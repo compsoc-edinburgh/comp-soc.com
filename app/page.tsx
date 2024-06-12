c604762b8a0d8865d3a047236f3995a56c0e0469
tomas <tomasroma64@gmail.com>
2024-06-12 15:10:07 +0100

    add variable size CompSoc title header image and subheading + performance improvement

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,20 @@
 'use client'
 
 import JoinDiscord from '@/components/JoinDiscord'
-import { useAnimatedBackground } from '@/context/AnimatedBackgroundContext'
-import { prefix } from '@/utils/prefix'
-import Image from 'next/image'
 import EventSection from '@/components/eventsection/EventSection'
 import FAQSection from '../components/faqsection/FAQSection'
 import SponsorSection from '@/components/sponsorsection/SponsorSection'
 import SigSection from '@/components/SIGCards/SigSection'
 import PresidentQuoteSection from '@/components/introSection/PresidentQuoteSection'
+import CompSocTitle from '@/components/CompSocTitle'
 
 export default function Home() {
-  const { toggleBackground } = useAnimatedBackground()
   return (
     <main className="bg-background flex items-center justify-center min-h-screen flex-col p-4 md:p-24">
       <div className="w-full">
         <div className="flex flex-col justify-between items-center h-96">
-          <Image
-            src={`${prefix}/compsoc-long.png`}
-            alt="Wide CompSoc logo"
-            width={512}
-            height={126}
-            className="filter drop-shadow-white"
-            style={{
-              filter:
-                'drop-shadow(0px 0px 50px rgba(255, 255, 255, 0.1)) drop-shadow(0px 0px 197.8px rgba(255, 255, 255, 0.1))',
-            }}
-          />
+          <CompSocTitle />
+
           <JoinDiscord />
         </div>
 
---
+++ components/CompSocTitle.tsx
@@ -0,0 +1,38 @@
+import { prefix } from '@/utils/prefix'
+import Image from 'next/image'
+
+const CompSocTitle = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <Image
+        src={`${prefix}/compsoc-short.png`}
+        alt="CompSoc logo"
+        width={256}
+        height={256}
+        draggable={false}
+        className="block md:hidden"
+        style={{
+          filter:
+            'drop-shadow(0px 0px 50px rgba(255, 255, 255, 0.1)) drop-shadow(0px 0px 197.8px rgba(255, 255, 255, 0.1))',
+        }}
+      />
+      <Image
+        src={`${prefix}/compsoc-long.png`}
+        alt="CompSoc logo"
+        width={516}
+        height={256}
+        draggable={false}
+        className="hidden md:block"
+        style={{
+          filter:
+            'drop-shadow(0px 0px 50px rgba(255, 255, 255, 0.1)) drop-shadow(0px 0px 197.8px rgba(255, 255, 255, 0.1))',
+        }}
+      />
+      <p className="text-gray-400 text-sm md:text-lg text-center mt-4">
+        University of Edinburgh&apos;s Technology society
+      </p>
+    </div>
+  )
+}
+
+export default CompSocTitle
