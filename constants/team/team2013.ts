da9cf1f75925ff417b0ff69f6e717e6420aacd52
Vincent <35348750+VinceKaj@users.noreply.github.com>
2024-06-18 22:46:57 +0100

    Update team2013.ts

--- constants/team/team2013.ts
+++ constants/team/team2013.ts
@@ -2,66 +2,52 @@ import { Committee, Role, LinkType } from '@/types/team'
 
 export const team2013: Committee[] = [
   {
-    name: 'Angus',
-    surname: 'Pearson',
+    name: 'Paul M',
+    surname: 'Scherer',
     role: Role.PRESIDENT,
-    image: 'angusp-1.jpg',
-    links: [{ type: LinkType.WEBSITE, url: 'http://toaster.cc' }],
-  },
-  {
-    name: 'James',
-    surname: 'Friel',
-    role: Role.VICE_PRESIDENT,
-    image: 'james-friel-1.jpg',
+    image: '',
     // No URL provided
   },
   {
-    name: 'Aseem',
-    surname: 'Narang',
+    name: 'Chris',
+    surname: 'Swart',
+    role: Role.VICE_PRESIDENT,
+    image: '',
+    // No URL provided
+  },
+  {
+    name: 'Kimberley',
+    surname: 'McCann',
     role: Role.SECRETARY,
     image: '',
     // No URL provided
   },
   {
-    name: 'Hugh',
-    surname: 'McGrade',
+    name: 'Martin',
+    surname: 'Husbyn',
     role: Role.TREASURER,
     image: '',
     // No URL provided
   },
   {
-    name: 'Rob',
-    surname: 'Tully',
+    name: 'Rikki "Teh Numbers"',
+    surname: 'Guy',
     role: Role.TECHNICAL_SECRETARY,
     image: '',
     // No URL provided
   },
   {
-    name: 'Hamish',
-    surname: 'Hutchings',
+    name: 'Craig',
+    surname: 'Ross',
     role: Role.SOCIAL_SECRETARY,
     image: '',
     // No URL provided
   },
   {
-    name: 'Lisa',
-    surname: 'Xie',
-    role: Role.SECOND_YEAR_REP,
+    name: 'Aseem',
+    surname: 'Narang',
+    role: Role.FIRST_YEAR_REP,
     image: '',
     // No URL provided
-  },
-  {
-    name: 'Amy',
-    surname: 'Gainford',
-    role: Role.SECOND_YEAR_REP,
-    image: '',
-    // No URL provided
-  },
-  {
-    name: 'Lewis',
-    surname: 'Barker',
-    role: Role.THIRD_YEAR_REP,
-    image: '',
-    // No URL provided
-  },
+  }
 ]
