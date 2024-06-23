051b5d275d0500004f8a5714ecab4cb22f5480b0
tomasmaillo <38633386+tomasmaillo@users.noreply.github.com>
2024-06-23 17:37:19 +0000

    Deploying to public from @ compsoc-edinburgh/comp-soc.com@509f0e8102958564595614c72109e5e51cfecf43 🚀

--- _next/static/chunks/app/minutes/[slug]/page-e298acbbe84ebbee.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{773:function(n,e,t){Promise.resolve().then(t.t.bind(t,50053,23)),Promise.resolve().then(t.t.bind(t,231,23))},50053:function(){}},function(n){n.O(0,[863,231,971,23,744],function(){return n(n.s=773)}),_N_E=n.O()}]);
