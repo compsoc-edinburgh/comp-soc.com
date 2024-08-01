1ca8ef356f11f57f4976b6fcfc2160cb989d8303
tomas <tomasroma64@gmail.com>
2024-08-01 18:17:20 +0100

    replace IG link for Discord and hide particles when out of viewport!

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,8 +1,7 @@
-import { DISCORD_INVITE_URL, INSTAGRAM_URL } from '@/constants/socials'
+import { DISCORD_INVITE_URL } from '@/constants/socials'
 import { motion } from 'framer-motion'
-import { FC } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import prefersReducedMotion from '@/utils/prefersReducedMotion'
-import { Instagram } from 'iconoir-react'
 import { prefix } from '@/utils/prefix'
 import Image from 'next/image'
 
@@ -23,15 +22,15 @@ const Particle: FC<{
   const particleVariants = {
     hidden: {
       opacity: 0,
-      x: 0,
-      y: 0,
+      x: x,
+      y: y,
       duration: 10,
       delay: delay,
     },
     visible: {
       opacity: [0, 0.8, 0],
-      x: x,
-      y: y,
+      x: 0,
+      y: 0,
       transition: {
         duration: 2,
         delay: Math.random() * 2,
@@ -54,7 +53,7 @@ const Particle: FC<{
 
   return (
     <motion.div
-      className="absolute w-2 h-2 bg-csred rounded-sm"
+      className="absolute w-2 h-2 bg-discordPurple rounded-sm"
       initial="hidden"
       variants={particleVariants}
       // animate={isHovered ? 'hover' : 'visible'}
@@ -65,12 +64,82 @@ const Particle: FC<{
 
 const Particles: FC = () => {
   if (prefersReducedMotion()) return null
+
+  const [isInView, setIsInView] = useState(false)
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsInView(entry.isIntersecting)
+      },
+      { threshold: 0.1 }
+    )
+
+    if (ref.current) {
+      observer.observe(ref.current)
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current)
+      }
+    }
+  }, [])
+
   return (
-    <>
-      {PARTICLES_COUNT.map(({ index, x, y, delay, duration }) => (
-        <Particle key={index} x={x} y={y} delay={delay} duration={duration} />
-      ))}
-    </>
+    <div className="absolute" ref={ref}>
+      {isInView &&
+        PARTICLES_COUNT.map(({ index, x, y, delay, duration }) => (
+          <Particle key={index} x={x} y={y} delay={delay} duration={duration} />
+        ))}
+    </div>
+  )
+}
+
+// TODO: If we ever run this website on server, we should cache this value!
+const MemberCount: FC = () => {
+  const [memberCount, setMemberCount] = useState<number | null>(null)
+  const [onlineCount, setOnlineCount] = useState<number | null>(null)
+
+  // return number with K, M, B, T, etc.
+  const formatMemberCount = (count: number) => {
+    if (count < 1000) return count
+    const exp = Math.floor(Math.log(count) / Math.log(1000))
+    return (count / Math.pow(1000, exp)).toFixed(1) + 'kMGTPEZY'[exp - 1]
+  }
+
+  useEffect(() => {
+    const fetchMemberCount = async () => {
+      const INVITE_CODE = DISCORD_INVITE_URL.split('/').pop()
+      try {
+        const response = await fetch(
+          `https://discord.com/api/v9/invites/${INVITE_CODE}?with_counts=true&with_expiration=true`
+        )
+        const data = await response.json()
+        setMemberCount(data.approximate_member_count)
+        setOnlineCount(data.approximate_presence_count)
+      } catch (error) {
+        console.error('Error fetching member count:', error)
+      }
+    }
+
+    fetchMemberCount()
+  }, [])
+
+  return (
+    <div
+      className="text-white text-center -translate-y-[2px]"
+      title={`There are ${onlineCount} members online`}
+    >
+      <p>Join our Discord!</p>
+
+      <p className="text-xs text-left text-gray-300">
+        {memberCount !== null
+          ? `${formatMemberCount(memberCount)} members online`
+          : 'CompSoc Loves You!'}
+      </p>
+    </div>
   )
 }
 
@@ -78,28 +147,13 @@ const SocialLinks: FC = () => {
   return (
     <div className="relative flex items-center justify-center gap-6">
       <Particles />
-      <motion.a
-        href={INSTAGRAM_URL}
-        target="_blank"
-        className="p-2 pr-3 text-white bg-csred rounded-lg z-10 shadow-lg flex items-center gap-1"
-        whileHover={{ scale: 1.05 }}
-      >
-        <Instagram width={48} height={48} />
 
-        <div className="text-white text-center">
-          <p>Stay up-to-date!</p>
-          <p className="text-xs text-left text-neutral-300">
-            Follow our Instagram
-          </p>
-        </div>
-      </motion.a>
-
+      <Particles />
       <motion.a
         href={DISCORD_INVITE_URL}
         target="_blank"
-        className="p-2 text-white bg-discordPurple rounded-lg z-10 shadow-lg flex justify-center items-center gap-1 h-16 w-16"
-        whileHover={{ scale: 1.05 }}
-        title="Join our Discord"
+        className="px-3 py-1 text-white bg-discordPurple rounded-lg z-10 shadow-lg flex items-center gap-2"
+        whileHover={{ scale: 1.1 }}
       >
         {/* 
           I would use this icon... but its the ugliest thing I've ever seen
@@ -107,11 +161,14 @@ const SocialLinks: FC = () => {
           So lets use a random svg
         */}
         <Image
+          className="translate-y-[2px]"
           src={`${prefix}/discord.svg`}
           alt="Discord logo"
-          width={36}
-          height={36}
+          width={48}
+          height={40}
         />
+
+        <MemberCount />
       </motion.a>
     </div>
   )
