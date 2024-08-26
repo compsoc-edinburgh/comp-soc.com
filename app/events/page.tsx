9c198472ac138a0ff3b10d58b793e7eabc68599d
Cat2005 <cmammola2005@gmail.com>
2024-08-26 10:49:38 +0100

    pushed events calendar to top

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -8,13 +8,6 @@ export default function Events() {
   return (
     <div className="lg:w-4/5 mx-auto">
       <div className="pt-4">
-        <Heading title="Flagship Events"></Heading>
-        <div className="mt-6 pb-6 font-space-mono">
-          These are big events we run every year without fail. Consider joining
-          us for one of these!
-        </div>
-        <EventCard></EventCard>
-
         <Heading title="Full event calendar"></Heading>
         <div className="mt-6 font-space-mono">
           Aside from the large events, CompSoc and its SIGs run a variety of
@@ -22,6 +15,15 @@ export default function Events() {
           what&apos;s coming up!
         </div>
         <EventsCalendar></EventsCalendar>
+
+        <div className="mt-10">
+          <Heading title="Flagship Events"></Heading>
+          <div className="mt-6 pb-6 font-space-mono">
+            These are big events we run every year without fail. Consider
+            joining us for one of these!
+          </div>
+          <EventCard></EventCard>
+        </div>
       </div>
     </div>
   )
