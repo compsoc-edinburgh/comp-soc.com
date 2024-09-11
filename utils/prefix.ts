3f08a27c3de82775131271918465e7ea07fe8707
Cat2005 <cmammola2005@gmail.com>
2024-09-11 09:30:50 +0100

    update prefix

--- utils/prefix.ts
+++ utils/prefix.ts
@@ -1,6 +1,4 @@
 const isProduction = process.env.NODE_ENV === 'production'
-const prefix = isProduction
-  ? 'https://compsoc-edinburgh.github.io/comp-soc.com'
-  : ''
+const prefix = isProduction ? 'https://comp-soc.com/' : ''
 
 export { prefix }
