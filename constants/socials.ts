ea281ff681cb46445038be5fdb8ee05b4ad30367
Cat2005 <cmammola2005@gmail.com>
2024-11-07 15:46:43 +0000

    fix socials consts

--- constants/socials.ts
+++ constants/socials.ts
@@ -1,3 +1,4 @@
 export const DISCORD_INVITE_URL = 'https://discord.gg/fmp7p9Ca4y'
 export const INSTAGRAM_URL = 'https://www.instagram.com/comp_soc/'
-export const LINKEIDN_URL = 'https://uk.linkedin.com/company/compsoc'
+export const LINKEDIN_URL = 'https://uk.linkedin.com/company/compsoc'
+export const X_URL = 'https://mobile.x.com/CompSoc'
