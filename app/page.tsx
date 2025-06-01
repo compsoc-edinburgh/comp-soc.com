d2e718aba521f4b321c7a16180f61f923ec003c2
J Cui <i@cjxol.com>
2025-06-01 18:59:31 +0100

    Add smooth scroll to the anchors on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,24 @@ import WelcomeSection from '@/components/welcomesection/WelcomeSection'
 import IntroSection from '@/components/introsection/IntroSection'
 import MoreResourcesSection from '@/components/moreResourcesSection/moreResourcesSection'
 // import SocialSection from '@/components/socialsection/SocialSection'
+import { useEffect } from 'react'
+import { scrollToAnchor } from '@/utils/scrollToAnchor'
 
 export default function Home() {
+  useEffect(() => {
+    const handleScrollToHash = () => {
+      const hash = window.location.hash.replace('#', '')
+      if (hash) {
+        scrollToAnchor(hash)
+      }
+    }
+    handleScrollToHash()
+    window.addEventListener('hashchange', handleScrollToHash)
+    return () => {
+      window.removeEventListener('hashchange', handleScrollToHash)
+    }
+  }, [])
+
   return (
     <main className="flex items-center justify-center min-h-screen flex-col max-w-5xl mx-auto pt-32">
       <div style={{ height: 'calc(100vh - 8rem)' }}>
