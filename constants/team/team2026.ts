33b2dcbd5ffb826ca0ad1e3a69abde72a81daca6
Skye <skyekaplynvasquez@gmail.com>
2026-05-26 00:31:51 +0100

    Added 2026 Committee
    	modified:   constants/team/team2025.ts
    	new file:   constants/team/team2026.ts
    
    Pushed updates for 2026 committee
    
    	modified:   constants/team/index.ts
    	modified:   constants/team/team2025.ts
    	new file:   constants/team/team2026.ts
    	new file:   public/team/aarav-2026-1.png
    	new file:   public/team/anya-2026-1.png
    	new file:   public/team/aryan-2026-1.png
    	new file:   public/team/hugo-2026-1.png
    	new file:   public/team/iretemi-2026-1.png
    	new file:   public/team/kaiwen-2026-1.png
    	new file:   public/team/luc-2026-1.png
    
    	modified:   constants/team/team2026.ts
    	constants/team/pui-2026-1.png
    
    	new file:   constants/team/pui-2026-1.png
    
    	modified:   constants/team/index.ts
    	new file:   constants/team/pui-2026-1.png
    	modified:   constants/team/team2025.ts
    	new file:   constants/team/team2026.ts
    	new file:   public/team/aarav-2026-1.png
    	new file:   public/team/anya-2026-1.png
    	new file:   public/team/aryan-2026-1.png
    	new file:   public/team/hugo-2026-1.png
    	new file:   public/team/iretemi-2026-1.png
    	new file:   public/team/kaiwen-2026-1.png
    	new file:   public/team/luc-2026-1.png
    
    	renamed:    constants/team/pui-2026-1.png -> public/team/pui-2026-1.png
    
    Changes to be committed:
    	modified:   constants/team/index.ts
    	modified:   constants/team/team2025.ts
    	new file:   constants/team/team2026.ts
    	new file:   public/team/aarav-2026-1.png
    	new file:   public/team/anya-2026-1.png
    	new file:   public/team/aryan-2026-1.png
    	new file:   public/team/hugo-2026-1.png
    	new file:   public/team/iretemi-2026-1.png
    	new file:   public/team/kaiwen-2026-1.png
    	new file:   public/team/luc-2026-1.png
    	new file:   public/team/pui-2026-1.png

---
+++ constants/team/team2026.ts
@@ -0,0 +1,262 @@
+import { Committee, LinkType, Role } from '@/lib/committee'
+
+export const team2026: Committee[] = [
+  {
+    name: 'Kaiwen',
+    surname: 'Wang',
+    role: Role.PRESIDENT,
+    image: 'kaiwen-2026-1.png',
+    sponsorContact: true,
+    sponsorEmail: 'president@comp-soc.com',
+    links: [
+    //  {
+    //    type: LinkType.LINKEDIN,
+    //    url: 'https://www.linkedin.com/in/kacper-szymanski-edi/',
+    //  },
+    ],
+  },
+  {
+    name: 'Luc',
+    surname: 'Wallace',
+    role: Role.VICE_PRESIDENT,
+    sponsorContact: true,
+    image: 'luc-2026-1.png',
+    links: [
+    //  {
+    //    type: LinkType.LINKEDIN,
+    //    url: 'https://www.linkedin.com/in/anna-petrusenko-0600b02b6/',
+    //  },
+    //  {
+    //    type: LinkType.INSTAGRAM,
+    //    url: 'https://www.instagram.com/petrusenkoanna',
+    //  },
+    ],
+  },
+  {
+    name: 'Dhyey',
+    surname: 'Mehta',
+    role: Role.TREASURER,
+    image: 'Dhyey-1.webp',
+    links: [
+    //  {
+    //    type: LinkType.LINKEDIN,
+    //    url: 'https://www.linkedin.com/in/kameran-russell-238712203/',
+    //  },
+    ],
+  },
+  {
+    name: 'Aarav',
+    surname: 'Parin',
+    role: Role.SECRETARY,
+    image: 'aarav-2026-1.png',
+    links: [
+    //  {
+    //    type: LinkType.LINKEDIN,
+    //    url: 'https://www.linkedin.com/in/alexander-mark-davis/',
+    //  },
+    //  {
+    //    type: LinkType.WEBSITE,
+    //    url: 'https://alexdavis.tardis.ac/',
+    //  },
+    //  {
+    //    type: LinkType.GITHUB,
+    //    url: 'https://github.com/alexDavis28/',
+    //  },
+    ],
+  },
+    {
+        name: 'Skye',
+        surname: 'K. V.',
+        role: Role.TECHNICAL_SECRETARY,
+        //image: '',
+        sponsorContact: false,
+        sponsorEmail: 'techsec@comp-soc.com',
+        links: [
+            {
+                type: LinkType.WEBSITE,
+                url: 'https://kaplyn.xyz/'
+            }
+        ],
+    },
+  {
+    name: 'Hugo',
+    surname: 'Varity-Povoas',
+    role: Role.SOCIAL_SECRETARY,
+    image: 'hugo-2026-1.png',
+    links: [
+    //  {
+    //    type: LinkType.LINKEDIN,
+    //    url: 'https://www.linkedin.com/in/param-mans/',
+    //  },
+    //  {
+    //    type: LinkType.INSTAGRAM,
+    //    url: 'https://www.instagram.com/param_mans',
+    //  },
+    //  {
+    //    type: LinkType.GITHUB,
+    //    url: 'https://github.com/param-mans',
+    //  },
+    ],
+  },
+  {
+    name: 'Pui',
+    surname: 'Hong Freeman Chung',
+    role: Role.SOCIAL_MEDIA_OFFICER,
+    image: 'pui-2026-1.png',
+    links: [
+    //  {
+    //    type: LinkType.LINKEDIN,
+    //    url: 'https://www.linkedin.com/in/yi-man-chan-6aa001289/',
+    //  },
+    ],
+  },
+  {
+    name: 'Iretemi',
+    surname: 'Ogunlaja',
+    role: Role.GRAPHIC_DESIGNER,
+    image: 'iretemi-2026-1.png',
+    links: [
+    //  {
+    //    type: LinkType.LINKEDIN,
+    //    url: 'https://www.linkedin.com/in/banburycheese/',
+    //  },
+    //  {
+    //    type: LinkType.WEBSITE,
+    //    url: 'https://itskay.co',
+    //  },
+    //  {
+    //    type: LinkType.INSTAGRAM,
+    //    url: 'https://www.instagram.com/banbury_cheese/',
+    //  },
+    //  {
+    //    type: LinkType.TWITTER,
+    //    url: 'https://x.com/banbury_cheese',
+    //  },
+    ],
+  },
+  {
+    name: 'Aryan',
+    surname: 'Ajmera',
+    role: Role.SECOND_YEAR_REP,
+    image: 'aryan-2026-1.png',
+    links: [
+    //  {
+    //    type: LinkType.LINKEDIN,
+    //    url: 'https://www.linkedin.com/in/danyil-butov/',
+    //  },
+    //  {
+    //    type: LinkType.GITHUB,
+    //    url: 'https://github.com/HJyup',
+    //  },
+    //  {
+    //    type: LinkType.INSTAGRAM,
+    //    url: 'https://www.instagram.com/danyil.butov',
+    //  },
+    //  {
+    //    type: LinkType.WEBSITE,
+    //    url: 'https://danyilbutov.com',
+    //  },
+    ],
+  },
+  {
+    name: 'Abdulwahid',
+    surname: 'Yaich',
+    role: Role.THIRD_YEAR_REP,
+    //image: '',
+    links: [
+    //  {
+    //    type: LinkType.LINKEDIN,
+    //    url: 'https://www.linkedin.com/in/anya-k-a8856927b/',
+    //  },
+    ],
+  },
+  {
+    name: 'Anya',
+    surname: 'Krisanova',
+    role: Role.FOURTH_YEAR_REP,
+    image: 'anya-2026-1.png',
+    links: [
+    //  {
+    //    type: LinkType.LINKEDIN,
+    //    url: 'https://www.linkedin.com/in/vjanovec/',
+    //  },
+    //  {
+    //    type: LinkType.GITHUB,
+    //    url: 'https://github.com/vjanovec',
+    //  },
+    //  {
+    //    type: LinkType.INSTAGRAM,
+    //    url: 'https://www.instagram.com/vojtech.janovec/',
+    //  },
+    ],
+  },
+  {
+    name: 'Pa\'uk',
+    surname: '',
+    role: Role.OLD_PERSON_REP,
+    links: [],
+  },
+  {
+    name: 'Emily',
+    surname: 'M',
+    role: Role.EDI_REP,
+    image: 'emily-1.jpg',
+    links: [
+      {
+        type: LinkType.LINKEDIN,
+        url: 'https://www.linkedin.com/in/emilymillerxyz/',
+      },
+      {
+        type: LinkType.WEBSITE,
+        url: 'https://emilymiller.xyz',
+      },
+      {
+        type: LinkType.GITHUB,
+        url: 'https://github.com/python151',
+      },
+    ],
+  },
+  //{
+  //  name: 'Dhyey',
+  //  surname: 'Mehta',
+  //  role: Role.JUNIOR_TREASURER,
+  //  image: 'Dhyey-1.webp',
+  //  links: [
+    //  {
+    //    type: LinkType.LINKEDIN,
+    //    url: 'https://www.linkedin.com/in/dhyeym/',
+    //  },
+  //  ],
+  //},
+  //{
+  //  name: 'Kaiwen (Kevin)',
+  //  surname: 'Wang',
+  //  role: Role.FIRST_YEAR_REP,
+  //  image: 'Kevin-1.webp',
+  //  links: [
+    //  {
+    //    type: LinkType.LINKEDIN,
+    //    url: 'https://www.linkedin.com/in/wangkaiwen',
+    //  },
+    //  {
+    //    type: LinkType.INSTAGRAM,
+    //    url: 'https://www.instagram.com/kaiwen.wang_',
+    //  },
+  //  ],
+  //},
+  {
+    name: 'Farhaan',
+    surname: 'Mukarram',
+    role: Role.MASTERS_REP,
+    links: [
+      {
+        type: LinkType.LINKEDIN,
+        url: 'https://www.linkedin.com/in/farhaan-mukarram',
+      },
+      {
+        type: LinkType.GITHUB,
+        url: 'https://github.com/farhaan-mukarram ',
+      },
+    ],
+  }
+]
