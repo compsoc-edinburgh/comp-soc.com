fe6ac3b834b6232c701678f5836705b9930a4e91
Luc Wallace <144689311+lucwl@users.noreply.github.com>
2026-08-26 22:08:56 +0100

    Update TRC description

--- constants/sponsors.ts
+++ constants/sponsors.ts
@@ -8,7 +8,7 @@ export const sponsors: Sponsor[] = [
   //   description: 'Addepar is a wealth management platform that specializes in data aggregation, analytics, and reporting.',
   //   link: 'https://addepar.com/',
   // },
-/*
+  /*
   {
     name: 'Meta',
     img: 'meta-logo.png',
@@ -23,17 +23,18 @@ export const sponsors: Sponsor[] = [
     name: 'School of Informatics',
     img: 'inf-logo.png',
     tier: 'platinum',
-    description: 'The School of Informatics is a world-leading research instutition in artificial intelligence, computer science, data science and more.',
-    link: 'https://informatics.ed.ac.uk/'
+    description:
+      'The School of Informatics is a world-leading research instutition in artificial intelligence, computer science, data science and more.',
+    link: 'https://informatics.ed.ac.uk/',
   },
- 
+
   {
     name: 'G-Research',
     img: 'gresearch-logo.png',
     tier: 'gold',
     description:
       'G-Research is a leading quantitative research and technology company.',
-    link: 'https://www.gresearch.com/'
+    link: 'https://www.gresearch.com/',
   },
 
   {
@@ -42,7 +43,7 @@ export const sponsors: Sponsor[] = [
     tier: 'gold',
     description:
       'Qube Research & Technologies is a global multi strategy investment manager.',
-    link: 'https://www.qube-rt.com/'
+    link: 'https://www.qube-rt.com/',
   },
   // {
   //   name: 'Lloyds',
@@ -55,10 +56,11 @@ export const sponsors: Sponsor[] = [
     name: 'Bank of Scotland',
     img: 'bankofscotland-logo.svg',
     tier: 'gold',
-    description: 'Bank of Scotland is part of Lloyds Banking Group, one of the UK\'s leading financial services groups.',
-    link: 'https://www.bankofscotland.co.uk/'
+    description:
+      "Bank of Scotland is part of Lloyds Banking Group, one of the UK's leading financial services groups.",
+    link: 'https://www.bankofscotland.co.uk/',
   },
-/*
+  /*
   {
     name: 'Viridien',
     img: 'viridien-logo.png',
@@ -73,7 +75,7 @@ export const sponsors: Sponsor[] = [
     img: 'bloomberg-logo.png',
     tier: 'silver',
     description: 'Bloomberg is a financial software, data and media company.',
-    link: 'https://www.bloomberg.com/'
+    link: 'https://www.bloomberg.com/',
   },
 
   // {
@@ -87,7 +89,7 @@ export const sponsors: Sponsor[] = [
     img: 'janestreet-logo.png',
     tier: 'bronze',
     description: 'Jane Street is a research-driven trading firm.',
-    link: 'https://www.janestreet.com/'
+    link: 'https://www.janestreet.com/',
   },
 
   // {
@@ -103,9 +105,9 @@ export const sponsors: Sponsor[] = [
     img: 'optiver-logo.png',
     tier: 'bronze',
     description: 'Optiver is a global market maker.',
-    link: 'https://www.optiver.com/'
+    link: 'https://www.optiver.com/',
   },
-/*
+  /*
   {
     name: 'Neuphonic',
     img: 'neuphonic-logo.png',
@@ -121,23 +123,25 @@ export const sponsors: Sponsor[] = [
     img: 'tpp-logo.png',
     tier: 'bronze',
     description: 'TPP is a leading global provider of healthcare technology.',
-    link: 'https://tpp-careers.com/'
+    link: 'https://tpp-careers.com/',
   },
-  
+
   {
     name: 'Jump Trading',
     img: 'jumptrading-logo.svg',
     tier: 'bronze',
-    description: 'Jump Trading is a trading firm with focuses on algorithmic trading and solving market problems.',
-    link: 'https://www.jumptrading.com/'
+    description:
+      'Jump Trading is a trading firm with focuses on algorithmic trading and solving market problems.',
+    link: 'https://www.jumptrading.com/',
   },
-  
+
   {
     name: 'Huawei',
     img: 'huawei-logo.svg',
     tier: 'platinum',
-    description: 'Huawei is a leading global provider of information and communications technology (ICT) infrastructure and smart devices.',
-    link: 'https://www.huawei.com/'
+    description:
+      'Huawei is a leading global provider of information and communications technology (ICT) infrastructure and smart devices.',
+    link: 'https://www.huawei.com/',
   },
 
   // {
@@ -152,31 +156,35 @@ export const sponsors: Sponsor[] = [
     name: 'Hudson River Trading',
     img: 'hrt-logo.png',
     tier: 'platinum',
-    description: 'Hudson River Trading (HRT) is a leading quantitative trading firm at the forefront of technical innovation in global financial markets.',
-    link: 'https://www.hudsonrivertrading.com/'
+    description:
+      'Hudson River Trading (HRT) is a leading quantitative trading firm at the forefront of technical innovation in global financial markets.',
+    link: 'https://www.hudsonrivertrading.com/',
   },
 
   {
     name: 'InstaLILY',
     img: 'instalily-logo.png',
     tier: 'silver',
-    description: 'InstaLILY builds autonomous AI agents for the businesses that make, move and fix the physical economy.',
-    link: 'https://www.instalily.ai/'
+    description:
+      'InstaLILY builds autonomous AI agents for the businesses that make, move and fix the physical economy.',
+    link: 'https://www.instalily.ai/',
   },
 
   {
     name: 'Wordsmith AI',
     img: 'wordsmithai-logo.png',
     tier: 'silver',
-    description: 'Wordsmith AI is an Edinburgh-based company building the command centre for in-house legal teams.',
-    link: 'https://www.wordsmith.ai/'
+    description:
+      'Wordsmith AI is an Edinburgh-based company building the command centre for in-house legal teams.',
+    link: 'https://www.wordsmith.ai/',
   },
 
   {
     name: 'The Robotics Company',
     img: 'theroboticscompany-logo.png',
     tier: 'bronze',
-    description: 'The Robotics Company is solving deployment infrastructure for robots at scale',
-    link: 'https://www.theroboticscompany.com/'
-  }
+    description:
+      'The Robotics Company is solving deployment infrastructure for robots at scale.',
+    link: 'https://www.theroboticscompany.com/',
+  },
 ]
